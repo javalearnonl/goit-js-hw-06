c34215652783a39cb9a24d4ce1c7c4080189dfb2
maksmvc <124098393+maksmvc@users.noreply.github.com>
2023-04-23 18:46:16 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,44 @@
-const currentPagePath = window.location.pathname;
-let currentPageNumber = parseInt(currentPagePath.match(/\d+/)[0]);
-let nextPageNumber = currentPageNumber + 1;
-let nextPagePath = `goit-js-hw-06/task-${nextPageNumber}.html`;
 
-nextPageButton.addEventListener('click', () => {
-  window.location.href = nextPagePath;
-});
+  const nextPageButton = document.querySelector('[data-action="next-page"]');
+  const currentPagePath = window.location.pathname; 
+  let nextPagePath;
+  switch (currentPagePath) {
+    case '/task-01.html':
+      nextPagePath = '/task-02.html';
+      break;
+    case '/task-02.html':
+      nextPagePath = '/task-03.html';
+      break;
+    case '/task-03.html':
+      nextPagePath = '/task-04.html';
+      break;
+    case '/task-04.html':
+      nextPagePath = '/task-05.html';
+      break;
+    case '/task-05.html':
+      nextPagePath = '/task-06.html';
+      break;
+    case '/task-06.html':
+      nextPagePath = '/task-07.html';
+      break;
+    case '/task-07.html':
+      nextPagePath = '/task-08.html';
+      break;
+    case '/task-08.html':
+      nextPagePath = '/task-09.html';
+      break;
+    case '/task-09.html':
+      nextPagePath = '/task-10.html';
+      break;
+    default:
+    
+      nextPagePath = '/task-01.html';
+      break;
+  }
+
+  nextPageButton.addEventListener('click', () => {
+
+    window.location.href = nextPagePath;
+  });
+
+  
