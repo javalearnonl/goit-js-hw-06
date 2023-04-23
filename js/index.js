89f4627f212b72111f5318d6cfded1e6de88661c
maksmvc <124098393+maksmvc@users.noreply.github.com>
2023-04-23 18:18:27 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,32 +3,32 @@
   const currentPagePath = window.location.pathname; 
   let nextPagePath;
   switch (currentPagePath) {
-    case './task-01.html':
-      nextPagePath = './task-02.html';
+    case 'https://maksmvc.github.io/goit-js-hw-06/task-01.html':
+      nextPagePath = 'https://maksmvc.github.io/goit-js-hw-06/task-02.html';
       break;
-    case './task-02.html':
-      nextPagePath = './task-03.html';
+    case 'https://maksmvc.github.io/goit-js-hw-06/task-02.html':
+      nextPagePath = 'https://maksmvc.github.io/goit-js-hw-06/task-03.html';
       break;
-    case './task-03.html':
-      nextPagePath = './task-04.html';
+    case 'https://maksmvc.github.io/goit-js-hw-06/task-03.html':
+      nextPagePath = 'https://maksmvc.github.io/goit-js-hw-06/task-04.html';
       break;
-    case './task-04.html':
-      nextPagePath = './task-05.html';
+    case 'https://maksmvc.github.io/goit-js-hw-06/task-04.html':
+      nextPagePath = 'https://maksmvc.github.io/goit-js-hw-06/task-05.html';
       break;
-    case './task-05.html':
-      nextPagePath = './task-06.html';
+    case 'https://maksmvc.github.io/goit-js-hw-06/task-05.html':
+      nextPagePath = 'https://maksmvc.github.io/goit-js-hw-06/task-06.html';
       break;
-    case './task-06.html':
-      nextPagePath = './task-07.html';
+    case 'https://maksmvc.github.io/goit-js-hw-06/task-06.html':
+      nextPagePath = 'https://maksmvc.github.io/goit-js-hw-06/task-07.html';
       break;
-    case './task-07.html':
-      nextPagePath = './task-08.html';
+    case 'https://maksmvc.github.io/goit-js-hw-06/task-07.html':
+      nextPagePath = 'https://maksmvc.github.io/goit-js-hw-06/task-08.html';
       break;
-    case './task-08.html':
-      nextPagePath = './task-09.html';
+    case 'https://maksmvc.github.io/goit-js-hw-06/task-08.html':
+      nextPagePath = 'https://maksmvc.github.io/goit-js-hw-06/task-09.html';
       break;
-    case './task-09.html':
-      nextPagePath = './task-10.html';
+    case 'https://maksmvc.github.io/goit-js-hw-06/task-09.html':
+      nextPagePath = 'https://maksmvc.github.io/goit-js-hw-06/task-10.html';
       break;
     default:
     
