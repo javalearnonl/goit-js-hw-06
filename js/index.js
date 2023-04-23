acbfe300cc0fa4e356ebfea674f95d544a22246f
maksmvc <124098393+maksmvc@users.noreply.github.com>
2023-04-23 18:34:51 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,7 @@
-const nextPageButton = document.querySelector('[data-action="next-page"]');
 const currentPagePath = window.location.pathname;
-const currentPageNumber = parseInt(currentPagePath.match(/\d+/)[0]);
-const nextPageNumber = currentPageNumber + 1;
-const nextPagePath = currentPagePath.replace(currentPageNumber, nextPageNumber);
+let currentPageNumber = parseInt(currentPagePath.match(/\d+/)[0]);
+let nextPageNumber = currentPageNumber + 1;
+let nextPagePath = `task-${nextPageNumber}.html`;
 
 nextPageButton.addEventListener('click', () => {
   window.location.href = nextPagePath;
