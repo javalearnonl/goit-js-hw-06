5d02550189593b0f161d6bd114a7d8c9a5ea3691
maksmvc <124098393+maksmvc@users.noreply.github.com>
2023-04-23 18:36:48 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const currentPagePath = window.location.pathname;
 let currentPageNumber = parseInt(currentPagePath.match(/\d+/)[0]);
 let nextPageNumber = currentPageNumber + 1;
-let nextPagePath = `task-${nextPageNumber}.html`;
+let nextPagePath = `goit-js-hw-06/task-${nextPageNumber}.html`;
 
 nextPageButton.addEventListener('click', () => {
   window.location.href = nextPagePath;
